7aa90477b4fdf334f517d98388ef16e2bc1f7939
ntatsuya <tatsuyanit@gmail.com>
2023-01-14 00:55:58 +0900

    cheatsheetの追加

---
+++ cheatsheet/stdinput_from_txt.py
@@ -0,0 +1,7 @@
+# input.txtに記述した内容を標準入力として受け取り実行する
+# 提出する時にはこの部分は含めない
+import io,sys
+with open("input.txt") as txt_opend:
+    TXT_INPUT = txt_opend.read()
+sys.stdin=io.StringIO(TXT_INPUT)
+# --------------------------------------------------------
