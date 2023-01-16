efe57d62ec36e635b3d2af8ce435b453a28bd984
ntatsuya <tatsuyanit@gmail.com>
2023-01-16 23:33:02 +0900

    数値計算のチートシート新規作成

---
+++ cheatsheet/calculation.py
@@ -0,0 +1,8 @@
+# from decimal import Decimal
+# from decimal import *
+# floatでは少数の丸め誤差等で意図しない数値になりがちなのでdecimalを使うことを検討する
+# Decimal('0.03') - Decimal('0.02')
+# > Decimal('0.01')
+
+# 分数を扱えるfractionもある
+# from fractions import Fraction
