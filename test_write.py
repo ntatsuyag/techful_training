ae601f38927738b80130d1aac28b4524727fcef7
ntatsuya <tatsuyanit@gmail.com>
2023-02-27 02:28:48 +0900

    改良

--- test_write.py
+++ test_write.py
@@ -4,10 +4,16 @@ import io,sys
 # 書き込みたい内容
 s = ""
 sl=[]
-to = 10**4
+to = 10**5
 for i in range(1,to+1):
     sl.append(str(i))
 s = " ".join(sl)
+
+s2 = ""
+sl2=[]
+for i in range(to,0,-1):
+    sl2.append(str(i))
+s2 = " ".join(sl2)
 
 # write
 with open("input.txt",mode='w') as txt_opend:
